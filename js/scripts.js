e94347b942874e6115bd9ee6aa349cd173efdab5
Dru Mumba <drumumba@Drus-MacBook-Pro.local>
2022-03-15 23:09:40 +0300

    Add prevent default event in js

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,17 @@
 
 
 /* UI Logic */
+$(document).ready(function(){
+	$("form.age").submit(function (event) {
+		event.preventDefault();
+	})
+
+	$("form.movies").submit(function (event) {
+		event.preventDefault();
+	})
+	
+	$("form.time").submit(function (event) {
+		event.preventDefault();
+	})
+
+});
