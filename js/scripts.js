2c96ee7e32d282c136bb9f8bedff4251c61808ae
Dru Mumba <drumumba@Drus-MacBook-Pro.local>
2022-03-16 22:07:37 +0300

    Add checkout.html

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,17 @@
 /* Business Logic */
+function validate() {
+	var age = document.getElementsByClassName("age-select");
+	var movie = document.getElementsByClassName("movies_content");
+	var tickets = document.getElementById("numb");
+	var time = document.getElementById("time-select");
 
+	if (age.value == "" || movie.value == "" || tickets.value == "" || time.value == "") {
+		alert("Please fill in all fields");
+		return false;
+	}else {
+		document.location.href="checkout.html"
+	}
+}
 
 /* UI Logic */
 $(document).ready(function(){
