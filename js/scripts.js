4838ac216acb8b7ee7c13bf22636997d8756e7d4
Dru Mumba <drumumba@Drus-MacBook-Pro.local>
2022-03-16 12:02:58 +0300

    Add hide and show functions for movie content

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,15 @@
 /* Business Logic */
 
 
-
 /* UI Logic */
 $(document).ready(function(){
 	$("form.customer-details").submit(function (event) {
 		event.preventDefault();
 	})
-	
+	$(".movies .movies_content").hide();
+	$(".age-select").click(function() {
+		var age_group = $(this).attr("data-radio");
+		$(".movies .movies_content").hide();
+		$("." + age_group).show();
+	});
 });
